2059ee773ebbe19c4e7a09a4ebc6ae494ccd77ec
泠漪 <32807696+AngelShadow2017@users.noreply.github.com>
2023-11-15 13:19:35 +0800

    Update contact.py

--- contact/contact.py
+++ contact/contact.py
@@ -110,6 +110,13 @@ class Contact:
                 if(len(self.searchList[i][cate])==0):
                     del self.searchList[i][cate]
             self.contacts[name][self.searchWords[i]]=[]
+    def deletePerson(self,name:str):
+        tmp = self.contacts.get(name)
+        if(not tmp):
+            return
+        self.removeAllPersonReversedList(name)
+        del self.contacts[name]#here we cant use tmp because tmp is going to return
+        return tmp
     def addPerson(self,name:str,mode:str,newDatas:list = []):
         def defaultMode():
             self.contacts[name]={
@@ -132,7 +139,7 @@ class Contact:
             if(not name in self.contacts):
                 defaultMode()
         else:
-            return
+            return True
         self.contacts[name]["tags"]["{{systemDefault"] = self.contacts[name]["tags"].get("{{systemDefault",[]);
         self.contacts[name]["tags"]["{{systemDefault"].append("{{Everyone")
         self.addPersonToType(name,"{{Everyone",self.CATEGORYDIR) #Everyone is everyone
@@ -207,6 +214,8 @@ class Console:
         #   -mode b/c/u (blend/cover/update)
         #  For F:
         #   -q search words
+        #  For M
+        #   -<n/p/e/c> data -o modify
         tmpValue = ""
         ind = 1
         while(ind<strLen):
@@ -279,10 +288,10 @@ class Console:
                         name = input("Enter Name >>> ")
                 else:
                     print("Name Invalid")
-                    return
+                    return True
             if(not self.con.contacts.get(name) and update):
                 print("User does not exists.")
-                return
+                return True
             if(not silence and self.con.contacts.get(name) and not cover):
                 print("Already exists, cover? (Y/N)")
                 if(self.con.getUserInput()):
@@ -310,6 +319,8 @@ class Console:
                         pVal = input("Categories (Press Enter To End): ")
                         if(len(pVal)==0):
                             break
+                        if(pField[:2]=="{{"):
+                            pField=pField[2:]
                         if(len(pField)==0):
                             pField="{{systemDefault"
                         create[2][pField] = create[2].get(pField,[])
@@ -334,6 +345,66 @@ class Console:
                 searchWords = curDataInput
             #print(searchWords)
             self.con.fuzzySearch(searchWords)
+        elif(mode == "M"):
+            moded = {}
+            lastCommand = "-n"
+            name = ""
+            phone = None#modify only one phone number
+            email = None#modify only one email number
+            oriInformation:dict = None
+            modTag = None
+            modTagOnce = None#mod a tag's which category
+            for t in types:
+                if(t[0]=="-o" and len(t[1])>0):
+                    moded[lastCommand] = t[1]
+                elif(t[0]=="-n" and len(t[1])>0):
+                    lastCommand=t[0]
+                    name = t[1][0]
+                elif((t[0]=="-p" or t[0]=="-e")):
+                    lastCommand=t[0]
+                    if(len(t[1])>0):
+                        if(t[0]=="-p"):
+                            phone=t[1][0]
+                        else:
+                            email=t[1][0]
+                elif(t[0]=="-c" and len(t[1])>0):
+                    lastCommand=t[0]
+                    modTag = t[1][0]
+                    if(len(t[1])>1):
+                        modTagOnce = t[1][1]
+            if(len(name)==0 or not self.con.contacts.get(name)):
+                return True
+            else:
+                def basicIndex(lst,val):
+                    l = len(lst)
+                    i_ = 0
+                    while(i_<l):
+                        if lst[i_] == val:
+                            return i_
+                        i_+=1
+                    return None
+                oriInformation = self.con.deletePerson(name)
+                if(moded.get("-n")):
+                    name=moded["-n"][0]
+                elif(moded.get("-p")):
+                    if(phone):
+                        pos = basicIndex(oriInformation["number"],phone)
+                        if(pos):
+                            oriInformation["number"][pos]=moded["-p"][0]
+                    else:
+                        oriInformation["number"]=moded["-p"]
+                elif(moded.get("-e")):
+                    if(email):
+                        '''
+                                "number":[],
+                                "email":[],
+                                "tags":{}
+                        '''
+                        pos = basicIndex(oriInformation["email"],phone)
+                        if(pos):
+                            oriInformation["email"][pos]=moded["-e"][0]
+                    else:
+                        oriInformation["email"]=moded["-e"]
         else:
             return False
         return True
